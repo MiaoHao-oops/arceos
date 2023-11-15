b00761fc919f52d5c226867309651ed5dc0ee6ef
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2023-11-15 14:47:04 +0800

    exercise1: `loader` support `ImgHeader` with app size info
    authored by: Miao Hao

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -1,18 +1,26 @@
 #![cfg_attr(feature = "axstd", no_std)]
 #![cfg_attr(feature = "axstd", no_main)]
 
+use core::mem::size_of;
+
 #[cfg(feature = "axstd")]
 use axstd::println;
 
 const PLASH_START: usize = 0x22000000;
 
+struct ImgHeader {
+    size: usize,
+}
+
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
-    let apps_start = PLASH_START as *const u8;
-    let apps_size = 32; // Dangerous!!! We need to get accurate size of apps.
+    let apps_start = (PLASH_START + size_of::<ImgHeader>()) as *const u8;
+    let img_header: &ImgHeader = unsafe { &*(PLASH_START as *const ImgHeader) };
+    let apps_size = img_header.size; // Dangerous!!! We need to get accurate size of apps.
 
     println!("Load payload ...");
 
+    println!("app size: {}", apps_size);
     let code = unsafe { core::slice::from_raw_parts(apps_start, apps_size) };
     println!("content: {:?}: ", code);
 

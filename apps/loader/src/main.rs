e9c1deab745301f45d1e8686a9a483d0695fe983
MiaoHao-oops <haomiao19@mails.ucas.ac.cn>
2023-11-15 21:26:51 +0800

    exercise3: loader support app execution
    - append a `ret` instruction, may be unnecessary
    - run apps in batch mode
    
    authored by: Miao Hao

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -1,5 +1,6 @@
 #![cfg_attr(feature = "axstd", no_std)]
 #![cfg_attr(feature = "axstd", no_main)]
+#![feature(asm_const)]
 
 use core::mem::size_of;
 
@@ -7,6 +8,7 @@ use core::mem::size_of;
 use axstd::println;
 
 const PLASH_START: usize = 0x22000000;
+const RUN_START: usize = 0xffff_ffc0_8010_0000;
 
 struct ImgHeader {
     app_num: usize,
@@ -22,7 +24,7 @@ fn main() {
     let app_header_size = size_of::<AppHeader>();
     let img_header: &ImgHeader = unsafe { &*(PLASH_START as *const ImgHeader) };
     let app_num = img_header.app_num;
-    let mut apps_start = PLASH_START + img_header_size + app_num * app_header_size;
+    let mut load_start = PLASH_START + img_header_size + app_num * app_header_size;
 
     println!("Load payload ...");
 
@@ -30,14 +32,34 @@ fn main() {
         let app_header: &AppHeader = unsafe {
             &*((PLASH_START + img_header_size + i * app_header_size) as *const AppHeader)
         };
-        let app_size = app_header.app_size;
+        let load_size = app_header.app_size;
         
-        println!("app[{}] size: {}", i, app_size);
-        let code = unsafe { 
-            core::slice::from_raw_parts(apps_start as *const u8, app_size) 
+        println!("app[{}] size: {}", i, load_size);
+        let load_code = unsafe {
+            core::slice::from_raw_parts(load_start as *const u8, load_size)
         };
-        println!("content: {:?}", code);
-        apps_start += app_size;
+        let run_code = unsafe {
+            core::slice::from_raw_parts_mut(RUN_START as *mut u8, load_size)
+        };
+        run_code.copy_from_slice(load_code);
+        println!("run code {:?}; address [{:?}]", run_code, run_code.as_ptr());
+
+        // FIXME: Do a code instrumentation, may be unnecessary
+        let ret_inst = unsafe {
+            core::slice::from_raw_parts_mut((RUN_START + load_size) as *mut u8, 4)
+        };
+        let ret: [u8; 4] = [0x6f, 0x00, 0x00, 0x00];
+        ret_inst.copy_from_slice(&ret);
+
+        println!("Execute app ...");
+        // execute app
+        unsafe { core::arch::asm!("
+            li      t2, {run_start}
+            jalr    t2",
+            run_start = const RUN_START,
+        )}
+
+        load_start += load_size;
     }
 
     println!("Load payload ok!");
